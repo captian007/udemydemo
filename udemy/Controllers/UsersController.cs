821f007129ff1a9d735aa21ea967537b57b83ffd
Captian007 <chetantherala@outlook.com>
2020-10-29 09:17:34 -0400

    Initial commit

---
+++ udemy/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using udemy.Data;
+using udemy.Entities;
+
+namespace udemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserbyId(int id)
+        {
+            var users = await _context.Users.FindAsync(id);
+            return users;
+        }
+
+
+
+    }
+}
---
+++ udemy/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using udemy.Entities;
+
+namespace udemy.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
